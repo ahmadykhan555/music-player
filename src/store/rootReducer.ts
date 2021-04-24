801b2544b7eb3b913f9144ed211abeb2f87813c5
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-24 05:04:57 +0200

    Expose appstate type

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,4 +2,5 @@ import songsState from "./songs/reducer";
 import { combineReducers } from "redux";
 
 const reducer = combineReducers({ songsState });
+export type AppState = ReturnType<typeof reducer>;
 export default reducer;
