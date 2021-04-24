21c10dea3febdfbf37819ab4a531fc76e23a1333
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-24 07:21:26 +0200

    Wire up like api

--- src/api/music.ts
+++ src/api/music.ts
@@ -6,8 +6,10 @@ export const getSongs = async (): Promise<Song[]> => {
 };
 
 export const likeSong = async (id: string): Promise<any> => {
+  const params = new URLSearchParams();
+  params.append("id", id);
   return httpPOST(
     "https://api-stg.jam-community.com/interact/like?apikey=___agAFTxkmMIWsmN9zOpM_6l2SkZPPy21LGRlxhYD8",
-    { id }
+    params
   );
 };
