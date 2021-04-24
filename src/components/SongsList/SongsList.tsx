63a1b6cc746e3eaf3d7c331616868bf69f1188df
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-24 06:35:46 +0200

    Create component for songs list

---
+++ src/components/SongsList/SongsList.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { Song } from "../../models/song";
+import { AppState } from "../../store/rootReducer";
+import { setPlayerState, setPlayingNow } from "../../store/songs/actions";
+import { PlayerStates } from "../../store/songs/types";
+import SongListItem from "../SongListItem/SongListItem";
+import "./SongsList.scss";
+
+interface OwnProps extends PropsFromRedux {}
+const SongsList: React.FC<OwnProps> = ({ songs, dispatch }) => {
+  const handleSongItemClicked = (songState: PlayerStates, song: Song) => {
+    songState === PlayerStates.Paused ? pauseSong() : playSong(song);
+  };
+
+  const pauseSong = () => dispatch(setPlayerState(PlayerStates.Paused));
+  const playSong = (song: Song) => {
+    dispatch(setPlayingNow(song));
+    dispatch(setPlayerState(PlayerStates.Playing));
+  };
+
+  return (
+    <div className='songs-list-component'>
+      {songs.length > 0 ? (
+        songs.map((song) => (
+          <SongListItem song={song} onSongItemClicked={handleSongItemClicked} />
+        ))
+      ) : (
+        <p>No songs yet</p>
+      )}
+    </div>
+  );
+};
+const mapStateToProps = (state: AppState) => ({
+  songs: state.songsState.songs,
+});
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+export default connector(SongsList);
