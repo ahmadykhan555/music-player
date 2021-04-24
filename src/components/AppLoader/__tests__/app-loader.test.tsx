d374618dbabef34d7e47a486fce70f2a6b76e1d1
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-25 01:20:52 +0200

    Added unit test for app loader

---
+++ src/components/AppLoader/__tests__/app-loader.test.tsx
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom/extend-expect";
+import { screen, render, cleanup } from "@testing-library/react";
+import AppLoader from "../AppLoader";
+
+afterEach(() => cleanup());
+
+test("should render app loader", () => {
+  render(<AppLoader />);
+  const el = screen.getByTestId("app-loader");
+  expect(el).toBeInTheDocument();
+});
+
+test("should render app loader with custom label", () => {
+  render(<AppLoader loaderText='hello from test' />);
+  const el = screen.getByTestId("app-loader");
+  expect(el).toHaveTextContent("hello from test");
+});
