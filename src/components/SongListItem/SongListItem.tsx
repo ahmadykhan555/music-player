ea968623a2a21e50f87ec9019eabe5a3c041543a
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-24 21:53:36 +0200

    Added like/unlike button to song-list-item component

--- src/components/SongListItem/SongListItem.tsx
+++ src/components/SongListItem/SongListItem.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Song } from "../../models/song";
 import "./SongListItem.scss";
-import playIcon from "../../asstes/images/play-icon.svg";
-import pauseIcon from "../../asstes/images/pause-icon.svg";
+import playIcon from "../../assets/images/play-icon.svg";
+import pauseIcon from "../../assets/images/pause-icon.svg";
+import heart from "../../assets/images/heart.png";
+import heartFilled from "../../assets/images/heart-filled.png";
 import { connect, ConnectedProps } from "react-redux";
 import { AppState } from "../../store/rootReducer";
 import { PlayerStates } from "../../store/songs/types";
 interface OwnProps extends PropsFromRedux {
   song: Song;
   onSongItemClicked: (songState: PlayerStates, song: Song) => void;
+  onSongLiked: (liked: boolean, id: string) => void;
+  songLiked: boolean;
 }
 const SongListItem: React.FC<OwnProps> = ({
   song,
   onSongItemClicked,
   playingNow,
   playerState,
+  songLiked = false,
+  onSongLiked,
 }) => {
+  const [liked, setLiked] = useState<boolean>(songLiked);
+  const handleSongLiked = (liked: boolean) => {
+    setLiked(liked);
+    onSongLiked(liked, song.id);
+  };
+
   return (
     <div className='song-list-item'>
       <div className='song-list-item__controls'>
@@ -36,6 +48,21 @@ const SongListItem: React.FC<OwnProps> = ({
       <div className='song-list-item__song-info'>
         <h3 className='song-list-item__song-info__name'>{song.name}</h3>
         <p className='song-list-item__song-info__artist'>{song.artist.name}</p>
+      </div>
+      <div className='song-list-item__controls'>
+        {liked ? (
+          <button
+            className='song-list-item__controls--like'
+            onClick={() => handleSongLiked(false)}>
+            <img src={heartFilled} />
+          </button>
+        ) : (
+          <button
+            className='song-list-item__controls--like'
+            onClick={() => handleSongLiked(true)}>
+            <img src={heart} />
+          </button>
+        )}
       </div>
     </div>
   );
