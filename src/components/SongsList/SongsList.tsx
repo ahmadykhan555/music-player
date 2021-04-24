e4c6569e85ecc48655ec44404cfa6493f9d49425
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-24 21:54:23 +0200

    Wireup api and persist liked songs in localstorage

--- src/components/SongsList/SongsList.tsx
+++ src/components/SongsList/SongsList.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { connect, ConnectedProps } from "react-redux";
+import { likeSong } from "../../api/music";
 import { Song } from "../../models/song";
 import { AppState } from "../../store/rootReducer";
 import { setPlayerState, setPlayingNow } from "../../store/songs/actions";
 import { PlayerStates } from "../../store/songs/types";
+import {
+  getFromLocalStorage,
+  LocalStorageKeys,
+  saveInLocalStorage,
+} from "../../utility/localStorage";
 import SongListItem from "../SongListItem/SongListItem";
 import "./SongsList.scss";
 
 interface OwnProps extends PropsFromRedux {}
 const SongsList: React.FC<OwnProps> = ({ songs, dispatch }) => {
+  const list = getFromLocalStorage(LocalStorageKeys.LikedSongsList) || {};
   const handleSongItemClicked = (songState: PlayerStates, song: Song) => {
     songState === PlayerStates.Paused ? pauseSong() : playSong(song);
+  };
+
+  const handleSongLiked = async (liked: boolean, songId: string) => {
+    saveInLocalStorage(LocalStorageKeys.LikedSongsList, {
+      ...list,
+      [songId]: liked,
+    });
+    if (liked) {
+      await likeSong(songId);
+    }
   };
 
   const pauseSong = () => dispatch(setPlayerState(PlayerStates.Paused));
@@ -23,7 +40,13 @@ const SongsList: React.FC<OwnProps> = ({ songs, dispatch }) => {
     <div className='songs-list-component'>
       {songs.length > 0 ? (
         songs.map((song) => (
-          <SongListItem song={song} onSongItemClicked={handleSongItemClicked} />
+          <SongListItem
+            key={song.id}
+            song={song}
+            onSongItemClicked={handleSongItemClicked}
+            onSongLiked={handleSongLiked}
+            songLiked={list[song.id]}
+          />
         ))
       ) : (
         <p>No songs yet</p>
