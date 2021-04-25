c1c677a64a400aceadac551394e6d79e2387b0ca
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-25 06:40:08 +0200

    Make localStorage utility robust and more readable

--- src/utility/localStorage.ts
+++ src/utility/localStorage.ts
@@ -1,14 +1,33 @@
+/**
+ * Initializes local storage with default values if any
+ */
 export const initLocalStorage = () => {
   if (!getFromLocalStorage(LocalStorageKeys.LikedSongsList)) {
     saveInLocalStorage(LocalStorageKeys.LikedSongsList, {});
   }
 };
 
-export const saveInLocalStorage = (key: string, payload: any) => {
+/**
+ *
+ * @param key identifier
+ * @param payload data
+ */
+export const saveInLocalStorage = (key: string, payload: any = "") => {
+  if (!key) {
+    return;
+  }
   localStorage.setItem(key, JSON.stringify(payload));
 };
 
+/**
+ *
+ * @param key identifier
+ * @returns
+ */
 export const getFromLocalStorage = (key: string) => {
+  if (!key || (key && !key.trim())) {
+    return null;
+  }
   return JSON.parse(localStorage.getItem(key) || "");
 };
 
