2d44443565a297785372a1fb28f35b2dd1c6887e
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-25 02:11:31 +0200

    Unit tests for music player

---
+++ src/__tests__/music.player.test.tsx
@@ -0,0 +1,52 @@
+import "@testing-library/jest-dom/extend-expect";
+import { screen, render, cleanup } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { getSongs } from "../api/music";
+import MusicPlayer from "../components/MusicPlayer/MusicPlayer";
+import { Song } from "../models/song";
+import store from "../store";
+import { setPlayerState, setPlayingNow } from "../store/songs/actions";
+import { PlayerStates } from "../store/songs/types";
+
+window.HTMLMediaElement.prototype.load = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.play = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.pause = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.addTextTrack = () => {
+  /* do nothing */
+};
+
+afterEach(() => cleanup());
+const MUSIC_PLAYER_ID = "app-music-player";
+
+const ConnectMusicPlayer = () => (
+  <Provider store={store}>
+    <MusicPlayer />
+  </Provider>
+);
+
+const loadSongInState = () => {
+  return new Promise<Song>(async (resolve) => {
+    const songs = await getSongs();
+    const selectedSong = songs[0];
+    store.dispatch(setPlayingNow(selectedSong));
+    resolve(selectedSong);
+  });
+};
+
+test("music player should render without crashing", async () => {
+  await render(<ConnectMusicPlayer />);
+  expect(screen.getByTestId(MUSIC_PLAYER_ID)).toBeDefined();
+});
+
+test("music player loads selected song without crashing", async () => {
+  await render(<ConnectMusicPlayer />);
+  const selectedSong = await loadSongInState();
+  const audioPlayer = document.getElementsByTagName("audio")[0];
+  expect(audioPlayer.src).toEqual(selectedSong.music_file_path);
+});
